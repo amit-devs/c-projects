e7271cabfdde7281c96093b53dd1a556097b5a6c
Amit <amitbtiwari05@gmail.com>
2026-01-28 22:48:08 +0530

    Implement calculator version 1 with menu and validation

--- calculator/main.c
+++ calculator/main.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
 int main(){
-    printf ("Calculator project setup done.\n");
+    int a,b,c;
+    char sym;
+    printf("Enter the value of A and B:");
+    scanf("%d%d",&a,&b);
+    printf("You Entered:\nA:%d\nB:%d\n",a,b);
+    printf("Enter the num for the calculation:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
+    scanf(" %c",&sym);
+    switch(sym){
+        case '+':{
+            c=a+b;
+            printf("Addition of C=%d",c);
+            break;
+
+        }
+        case '-':{
+            c=a-b;
+            printf("Subtraction of C=%d",c);
+            break;
+        }
+        case '*':{
+            c=a*b;
+            printf("Multiplication of C=%d",c);
+            break;
+
+        }
+        case '/':{
+            c=a/b;
+            printf("Division of C=%d",c);
+            break;
+
+        }
+        default:{
+            printf("Please enter a valid number");
+            break;
+
+        }
+    }
     return 0;
 }
